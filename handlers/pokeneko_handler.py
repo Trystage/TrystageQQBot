8ea0807658c895cae3faae7bfa28c68629b9461e
4C01 t <Catcat-jia@foxmail.com>
2025-10-16 02:04:43 +0800

    index out o' bound
    saaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaad

--- handlers/pokeneko_handler.py
+++ handlers/pokeneko_handler.py
@@ -4,6 +4,6 @@ from utils.websocket_utils import send_message
 
 async def handle_poke_neko(group_id, user_id, target_id, websocket):
     if user_id in SUPER_USER:
-        await send_message(websocket, POKE_SU_ANS[dice(len(POKE_SU_ANS),  114514^114514)], user_id, group_id)
+        await send_message(websocket, POKE_SU_ANS[dice(len(POKE_SU_ANS - 1),  114514^114514)], user_id, group_id)
     else:
-        await send_message(websocket, POKE_ANS[dice(len(POKE_ANS),  114514^114514)], user_id, group_id)
+        await send_message(websocket, POKE_ANS[dice(len(POKE_ANS - 1),  114514^114514)], user_id, group_id)
