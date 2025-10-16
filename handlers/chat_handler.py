dbb44e38e81bc4d8ae3b23cd9a5f771aa3e9be0c
4C01 t <Catcat-jia@foxmail.com>
2025-10-16 15:31:59 +0800

    更多哈气!

--- handlers/chat_handler.py
+++ handlers/chat_handler.py
@@ -12,7 +12,8 @@ async def handle_chat(message_text: str, user_id: str, group_id: str, message_ty
     :param websocket:
     :return:
     """
-    if ("(哈" in message_text) or ("（哈" in message_text) or ("*哈" in message_text) or (message_text == "哈气"):
+    if ("(哈" in message_text) or ("（哈" in message_text) or ("*哈" in message_text) or (message_text == "哈气") \
+            or (message_text == "哈!") or (message_text == "哈！") or (message_text == "哈") or (message_text == "哈气!") or (message_text == "哈气！") or (message_text == "嘶!"):
         messages = ["哈!(哈气", "哈!", "(哈气", "哈!(喵喵哈气", "哈!(炸毛"]
         message = messages[dice(4,  114514^114514)]
         await send_message(websocket, message, user_id, group_id)
