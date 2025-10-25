89c7c8dba5ce22fb2409c2598980a7a111042602
4C01 t <Catcat-jia@foxmail.com>
2025-10-25 22:49:24 +0800

    购买handleD10(14)修复

--- modules/chikari_yinpa/utils.py
+++ modules/chikari_yinpa/utils.py
@@ -559,7 +559,7 @@ class Utils:
             for i in l:
                 sk[2] += i[2]
             str += DHandles.skill_refresh(uid,sk[0],level = sk[2])
-        elif id == 13:
+        elif id == 14:
             d = Utils.dice(10,13)
             str += f"1d10 = {d}"
             if d == 1:
